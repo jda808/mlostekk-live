cfa7716b38446d5bfb536c43da26c7bb2d455bc2
mlostek@gmail.com <mlostek@gmail.com@174fb894-b733-5f89-b0cf-8dd91bf55e6b>
2012-11-28 00:01:59 +0000

    FINE WORKING VERSION NOW!!! (maybe there are smaller bugs, so test it!)

--- trunk/live/midiRemoteScripts/liveLaunchpad/SpecialMixerComponent.py
+++ trunk/live/midiRemoteScripts/liveLaunchpad/SpecialMixerComponent.py
@@ -9,60 +9,53 @@ class SpecialMixerComponent(MixerComponent):
 
 	def __init__(self, num_tracks, num_returns = 0, with_eqs = False, with_filters = False):
 		MixerComponent.__init__(self, num_tracks, num_returns, with_eqs, with_filters)
-		self._unarm_all_button = None
 		self._unsolo_all_button = None
 		self._unmute_all_button = None
-
+		self._stop_all_button = None
+		
 	" DISCONNECT "
 	def disconnect(self):
-		if (self._unarm_all_button != None):
-			self._unarm_all_button.remove_value_listener(self._unarm_all_value)
-			self._unarm_all_button = None
 		if (self._unsolo_all_button != None):
 			self._unsolo_all_button.remove_value_listener(self._unsolo_all_value)
 			self._unsolo_all_button = None
 		if (self._unmute_all_button != None):
 			self._unmute_all_button.remove_value_listener(self._unmute_all_value)
-			self._unmute_all_button = None
+			self._unmute_all_button = None 
+		if (self._stop_all_button != None):
+			self._stop_all_button.remove_value_listener(self._stop_all_value)
+			self._stop_all_button = None
 		MixerComponent.disconnect(self)
 
 	" SET GLOBAL BUTTONS "
-	def set_global_buttons(self, unarm_all, unsolo_all, unmute_all):
-		assert isinstance(unarm_all, (ButtonElement, type(None)))
+	def set_global_buttons(self, unmute_all, unsolo_all, stop_all):
+		assert isinstance(stop_all, (ButtonElement, type(None)))
 		assert isinstance(unsolo_all, (ButtonElement, type(None)))
-		assert isinstance(unmute_all, (ButtonElement, type(None)))
-		if (self._unarm_all_button != None):
-			self._unarm_all_button.remove_value_listener(self._unarm_all_value)
-		self._unarm_all_button = unarm_all
-		if (self._unarm_all_button != None):
-			self._unarm_all_button.add_value_listener(self._unarm_all_value)
-			self._unarm_all_button.turn_off()
+		assert isinstance(unmute_all, (ButtonElement, type(None)))		
+		
 		if (self._unsolo_all_button != None):
 			self._unsolo_all_button.remove_value_listener(self._unsolo_all_value)
-		self._unsolo_all_button = unsolo_all
+		self._unsolo_all_button = unsolo_all		
 		if (self._unsolo_all_button != None):
 			self._unsolo_all_button.add_value_listener(self._unsolo_all_value)
 			self._unsolo_all_button.turn_off()
+			
 		if (self._unmute_all_button != None):
-			self._unmute_all_button.remove_value_listener(self._unmute_all_value)
+			self._unmute_all_button.remove_value_listener(self._unmute_all_value)			
 		self._unmute_all_button = unmute_all
 		if (self._unmute_all_button != None):
 			self._unmute_all_button.add_value_listener(self._unmute_all_value)
 			self._unmute_all_button.turn_off()
+			
+		if (self._stop_all_button != None):
+			self._stop_all_button.remove_value_listener(self._stop_all_value)	
+		self._stop_all_button = stop_all
+		if (self._stop_all_button != None):
+			self._stop_all_button.add_value_listener(self._stop_all_value)
+			self._stop_all_button.turn_on()
 
 	" CREATE CHANNEL STRIP "
 	def _create_strip(self):
 		return DefChannelStripComponent()
-
-	" UNARM ALL "
-	def _unarm_all_value(self, value):
-		assert self.is_enabled()
-		assert (self._unarm_all_button != None)
-		assert (value in range(128))
-		if ((value != 0) or (not self._unarm_all_button.is_momentary())):
-			for track in self.song().tracks:
-				if (track.can_be_armed and track.arm):
-					track.arm = False
 
 	" UNSOLO ALL "
 	def _unsolo_all_value(self, value):
@@ -73,6 +66,8 @@ class SpecialMixerComponent(MixerComponent):
 			for track in (self.song().tracks + self.song().return_tracks):
 				if track.solo:
 					track.solo = False
+		else:
+			self._unsolo_all_button.turn_off()
 
 	" UNMUTE ALL "
 	def _unmute_all_value(self, value):
@@ -83,3 +78,17 @@ class SpecialMixerComponent(MixerComponent):
 			for track in (self.song().tracks + self.song().return_tracks):
 				if track.mute:
 					track.mute = False
+		else:
+			self._unmute_all_button.turn_off()
+			
+	""" STOP ALL """
+	def _stop_all_value(self, value):
+		assert self.is_enabled()
+		assert (self._stop_all_button != None)
+		assert (value in range(128))
+		if ((value != 0) or (not self._stop_all_button.is_momentary())):
+			for track in (self.song().tracks + self.song().return_tracks):
+				if track.mute:
+					track.mute = False
+		else:
+			self._stop_all_button.turn_off()
--- trunk/live/midiRemoteScripts/liveLaunchpad/SpecialMixerSelectorComponent.py
+++ trunk/live/midiRemoteScripts/liveLaunchpad/SpecialMixerSelectorComponent.py
@@ -5,10 +5,7 @@ from _Framework.SessionComponent import SessionComponent #@UnresolvedImport
 from SpecialMixerComponent import SpecialMixerComponent 
 from PreciseButtonSliderElement import * #@UnusedWildImport
 from consts import * #@UnusedWildImport
-
-PAN_VALUE_MAP = (-1.0, -0.63492099999999996, -0.31746000000000002, 0.0, 0.0, 0.31746000000000002, 0.63492099999999996, 1.0)
-VOL_VALUE_MAP = (0.0, 0.14288200000000001, 0.30241400000000002, 0.40000000000000002, 0.55000000000000004, 0.69999999999999996, 0.84999999999999998, 1.0)
-SEND_VALUE_MAP = (0.0, 0.103536, 0.164219, 0.23843900000000001, 0.34366400000000003, 0.55000000000000004, 0.77494200000000002, 1.0)
+from _liveUtils.Logger import log #@UnresolvedImport
 
 class SpecialMixerSelectorComponent(ModeSelectorComponent):
 	" CLASS THAT HANDLES DIFFERENT MIXER MODES "
@@ -24,7 +21,6 @@ class SpecialMixerSelectorComponent(ModeSelectorComponent):
 		self._session = session
 		self._mixer = SpecialMixerComponent(len(self.song().tracks))
 		self._matrix = matrix
-		self._sliders = []
 		self._mixer.name = "Mixer"
 		self._mixer.master_strip().name = "Master_Channel_strip"
 		self._mixer.selected_strip().name = "Selected_Channel_strip"
@@ -33,24 +29,15 @@ class SpecialMixerSelectorComponent(ModeSelectorComponent):
 			if self.song().tracks[column].is_foldable:
 				if button_index < GROUPS_CONSIDERED:
 					self._mixer.channel_strip(column).name = ("Channel_Strip_" + str(column))
-					self._sliders.append(PreciseButtonSliderElement(tuple([ matrix.get_button(button_index, (7 - row)) for row in range(8) ])))
-					self._sliders[-1].name = ("Button_Slider_" + str(button_index))
 					button_index = button_index + 1
-		self._side_buttons = side_buttons[4:]
+		self._side_buttons = side_buttons[5:]
 		self._update_callback = None
 		self._session.set_mixer(self._mixer)
-		self.set_modes_buttons(side_buttons[:4])
 
 	" DISCONNECT "
 	def disconnect(self):
-		for button in self._modes_buttons:
-			button.remove_value_listener(self._mode_value)
 		self._session = None
 		self._mixer = None
-		for slider in self._sliders:
-			slider.release_parameter()
-			slider.set_disabled(True)
-		self._sliders = None
 		self._matrix = None
 		self._side_buttons = None
 		self._update_callback = None
@@ -60,19 +47,6 @@ class SpecialMixerSelectorComponent(ModeSelectorComponent):
 	def set_update_callback(self, callback):
 		assert (dir(callback).count("im_func") is 1)
 		self._update_callback = callback
-
-	" SET THE MODE BUTTONS "
-	def set_modes_buttons(self, buttons):
-		assert ((buttons == None) or (isinstance(buttons, tuple) or (len(buttons) == self.number_of_modes())))
-		identify_sender = True
-		for button in self._modes_buttons:
-			button.remove_value_listener(self._mode_value)
-		self._modes_buttons = []
-		if (buttons != None):
-			for button in buttons:
-				assert isinstance(button, ButtonElement)
-				self._modes_buttons.append(button)
-				button.add_value_listener(self._mode_value, identify_sender)
 
 	" SET THE MODE "
 	def set_mode(self, mode):
@@ -91,13 +65,11 @@ class SpecialMixerSelectorComponent(ModeSelectorComponent):
 
 	" GET NUMBER OF MODES "
 	def number_of_modes(self):
-		return 4
+		return 1
 
 	" ENABLED CALLBACK "
 	def on_enabled_changed(self):
 		enabled = self.is_enabled()
-		for index in range(self._matrix.width()):
-			self._sliders[index].set_disabled((not enabled))
 		self._mixer.set_enabled(enabled)
 		self.set_mode(-1)
 
@@ -111,44 +83,22 @@ class SpecialMixerSelectorComponent(ModeSelectorComponent):
 						self._matrix.get_button(button_index, row).set_on_off_values(127, LED_OFF)
 						button_index = button_index + 1
 			strip = self._mixer.channel_strip(track)
-			strip.set_default_buttons(None, None, None, None)
 			strip.set_solo_button(None)
-			strip.set_arm_button(None)
-			strip.set_send_controls((None, None))
-			strip.set_pan_control(None)
-			strip.set_volume_control(None)
+			strip.set_mute_button(None)   
 		self._session.set_stop_track_clip_buttons(None)
 		self._mixer.set_global_buttons(None, None, None)
 		self._session.set_stop_all_clips_button(None)
 
 	" UPDATE ME "
 	def update(self):
-		assert (self._modes_buttons != None)
 		if self.is_enabled():
-			if (self._modes_buttons != None):
-				for index in range(len(self._modes_buttons)):
-					self._modes_buttons[index].set_on_off_values(GREEN_FULL, GREEN_THIRD)
-					if (index == self._mode_index):
-						self._modes_buttons[index].turn_on()
-					else:
-						self._modes_buttons[index].turn_off()
 			for button in self._side_buttons:
 				button.set_on_off_values(127, LED_OFF)
 				button.turn_off()
-			for index in range(self._matrix.width()):
-				self._sliders[index].set_disabled((self._mode_index == -1))
 			self._mixer.set_allow_update(False)
 			self._session.set_allow_update(False)
 			if (self._mode_index == -1):
 				self._setup_mixer_overview()
-			elif (self._mode_index == 0):
-				self._setup_volume_mode()
-			elif (self._mode_index == 1):
-				self._setup_pan_mode()
-			elif (self._mode_index == 2):
-				self._setup_send1_mode()
-			elif (self._mode_index == 3):
-				self._setup_send2_mode()
 			else:
 				assert False
 			if (self._update_callback != None):
@@ -160,119 +110,43 @@ class SpecialMixerSelectorComponent(ModeSelectorComponent):
 
 	" SETUP THE MIXER "
 	def _setup_mixer_overview(self):
-		trkon_index = 5
+		# mute and solo and stop
 		stop_buttons = []
 		button_index = 0
 		for track in range(len(self.song().tracks)):
-			if button_index < GROUPS_CONSIDERED:
-				if self.song().tracks[track].is_foldable:
+			if self.song().tracks[track].is_foldable:
+				if button_index < GROUPS_CONSIDERED:
 					strip = self._mixer.channel_strip(track)
-					strip.set_send_controls((None, None))
-					strip.set_pan_control(None)
-					strip.set_volume_control(None)
-					self._sliders[button_index].release_parameter()
-					for row in range(self._matrix.height()):
-						full_value = GREEN_THIRD
-						third_value = GREEN_FULL
-						if (row == trkon_index):
-							full_value = AMBER_FULL
-							third_value = AMBER_THIRD
-						elif (row > 3):
-							full_value = RED_FULL
-							third_value = RED_THIRD
-						self._matrix.get_button(button_index, row).set_on_off_values(full_value, third_value)
-					strip.set_default_buttons(self._matrix.get_button(button_index, 0), self._matrix.get_button(button_index, 1), self._matrix.get_button(button_index, 2), self._matrix.get_button(button_index, 3))
-					stop_buttons.append(self._matrix.get_button(button_index, 4))
-					strip.set_mute_button(self._matrix.get_button(button_index, 5))
-					strip.set_solo_button(self._matrix.get_button(button_index, 6))
-					strip.set_arm_button(self._matrix.get_button(button_index, 7))				
+					# mute
+					self._matrix.get_button(button_index, MUTE_INDEX).set_on_off_values(GREEN_FULL, GREEN_THIRD)
+					strip.set_mute_button(self._matrix.get_button(button_index, MUTE_INDEX))		
+					# solo
+					self._matrix.get_button(button_index, SOLO_INDEX).set_on_off_values(RED_FULL, RED_THIRD )			
+					strip.set_solo_button(self._matrix.get_button(button_index, SOLO_INDEX))
+					# stop				
+					button = self._matrix.get_button(button_index, STOP_INDEX)				
+					button.set_on_off_values(AMBER_FULL, AMBER_THIRD)				
+					button.turn_on();
+					stop_buttons.append(button)
+					# increment
 					button_index = button_index + 1
 				else:
 					stop_buttons.append(None)
-		for button in self._side_buttons:
-			if (list(self._side_buttons).index(button) == (trkon_index - 4)):
-				button.set_on_off_values(AMBER_FULL, AMBER_THIRD)
 			else:
-				button.set_on_off_values(RED_FULL, RED_THIRD)
-			button.set_force_next_value()
-			button.turn_off()
+				stop_buttons.append(None)				
+			
+		# side buttons
+		self._side_buttons[0].set_on_off_values(RED_FULL, RED_THIRD)
+		self._side_buttons[0].set_force_next_value()
+		self._side_buttons[0].turn_off()		
+		self._side_buttons[1].set_on_off_values(AMBER_FULL, AMBER_HALF)
+		self._side_buttons[1].set_force_next_value()
+		self._side_buttons[1].turn_off()
+		self._side_buttons[2].set_on_off_values(GREEN_FULL, GREEN_THIRD)
+		self._side_buttons[2].set_force_next_value()
+		self._side_buttons[2].turn_on()
+			
 		self._session.set_stop_track_clip_buttons(tuple(stop_buttons))
-		self._session.set_stop_all_clips_button(self._side_buttons[0])
-		self._mixer.set_global_buttons(self._side_buttons[3], self._side_buttons[2], self._side_buttons[1])
-
-	" SETUP THE VOLUME MODE "
-	def _setup_volume_mode(self):
-		for track in range(self._matrix.width()):
-			strip = self._mixer.channel_strip(track)
-			strip.set_default_buttons(None, None, None, None)
-			strip.set_mute_button(None)
-			strip.set_solo_button(None)
-			strip.set_arm_button(None)
-			strip.set_send_controls((None, None))
-			strip.set_pan_control(None)
-			for row in range(self._matrix.height()):
-				self._matrix.get_button(track, row).set_on_off_values(GREEN_FULL, LED_OFF)
-			self._sliders[track].set_mode(SLIDER_MODE_VOLUME)
-			self._sliders[track].set_value_map(VOL_VALUE_MAP)
-			strip.set_volume_control(self._sliders[track])
-		self._session.set_stop_track_clip_buttons(None)
-		self._session.set_stop_all_clips_button(None)
-		self._mixer.set_global_buttons(None, None, None)
-
-	" SETUP THE PAN MODE"
-	def _setup_pan_mode(self):
-		for track in range(self._matrix.width()):
-			strip = self._mixer.channel_strip(track)
-			strip.set_default_buttons(None, None, None, None)
-			strip.set_mute_button(None)
-			strip.set_solo_button(None)
-			strip.set_arm_button(None)
-			strip.set_send_controls((None, None))
-			strip.set_volume_control(None)
-			for row in range(self._matrix.height()):
-				self._matrix.get_button(track, row).set_on_off_values(AMBER_FULL, LED_OFF)
-			self._sliders[track].set_mode(SLIDER_MODE_PAN)
-			self._sliders[track].set_value_map(PAN_VALUE_MAP)
-			strip.set_pan_control(self._sliders[track])
-		self._session.set_stop_track_clip_buttons(None)
-		self._session.set_stop_all_clips_button(None)
-		self._mixer.set_global_buttons(None, None, None)
-
-	" SETUP THE SEND 1 MODE "
-	def _setup_send1_mode(self):
-		for track in range(self._matrix.width()):
-			strip = self._mixer.channel_strip(track)
-			strip.set_default_buttons(None, None, None, None)
-			strip.set_mute_button(None)
-			strip.set_solo_button(None)
-			strip.set_arm_button(None)
-			strip.set_volume_control(None)
-			strip.set_pan_control(None)
-			for row in range(self._matrix.height()):
-				self._matrix.get_button(track, row).set_on_off_values(RED_FULL, LED_OFF)
-			self._sliders[track].set_mode(SLIDER_MODE_VOLUME)
-			self._sliders[track].set_value_map(SEND_VALUE_MAP)
-			strip.set_send_controls((self._sliders[track], None))
-		self._session.set_stop_track_clip_buttons(None)
-		self._session.set_stop_all_clips_button(None)
-		self._mixer.set_global_buttons(None, None, None)
-
-	" SETUP THE SEND 2 MODE "
-	def _setup_send2_mode(self):
-		for track in range(self._matrix.width()):
-			strip = self._mixer.channel_strip(track)
-			strip.set_default_buttons(None, None, None, None)
-			strip.set_mute_button(None)
-			strip.set_solo_button(None)
-			strip.set_arm_button(None)
-			strip.set_volume_control(None)
-			strip.set_pan_control(None)
-			for row in range(self._matrix.height()):
-				self._matrix.get_button(track, row).set_on_off_values(RED_FULL, LED_OFF)
-			self._sliders[track].set_mode(SLIDER_MODE_VOLUME)
-			self._sliders[track].set_value_map(SEND_VALUE_MAP)
-			strip.set_send_controls((None, self._sliders[track]))
-		self._session.set_stop_track_clip_buttons(None)
-		self._session.set_stop_all_clips_button(None)
-		self._mixer.set_global_buttons(None, None, None)
-
+		self._session.set_mixer(self._mixer)
+		self._session.set_stop_all_clips_button(self._side_buttons[1])
+		self._mixer.set_global_buttons(self._side_buttons[2], self._side_buttons[0], self._side_buttons[1])
